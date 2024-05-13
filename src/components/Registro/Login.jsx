c0f4e0251506442fdf0a50ca83034f1ccfaa7c1e
LUCAS\monto <lucasmontoby@gmail.com>
2024-05-13 18:45:44 -0300

    finhis con mensaje de login

--- src/components/Registro/Login.jsx
+++ src/components/Registro/Login.jsx
@@ -7,6 +7,8 @@ const Login = () => {
     password: '',
   });
 
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // Estado para controlar si el usuario está logueado
+
   const handleChange = (e) => {
     setLoginData({ ...loginData, [e.target.name]: e.target.value });
   };
@@ -17,6 +19,7 @@ const Login = () => {
     try {
       const response = await axios.post('http://localhost:8081/auth/login', loginData);
       console.log(response.data);
+      setIsLoggedIn(true); // Marcar al usuario como logueado después de una respuesta exitosa
       // Aquí podrías manejar la respuesta, por ejemplo, guardar el token en el estado global.
     } catch (error) {
       console.error('Error al iniciar sesión:', error.response.data.error);
@@ -39,8 +42,58 @@ const Login = () => {
         <br />
         <button type="submit">Iniciar Sesión</button>
       </form>
+      {isLoggedIn && <p>¡Has iniciado sesión correctamente!</p>} {/* Mostrar mensaje emergente si el usuario está logueado */}
     </div>
   );
 };
 
 export default Login;
+
+
+
+// import React, { useState } from 'react';
+// import axios from 'axios';
+
+// const Login = () => {
+//   const [loginData, setLoginData] = useState({
+//     username: '',
+//     password: '',
+//   });
+
+//   const handleChange = (e) => {
+//     setLoginData({ ...loginData, [e.target.name]: e.target.value });
+//   };
+
+//   const handleLogin = async (e) => {
+//     e.preventDefault();
+
+//     try {
+//       const response = await axios.post('http://localhost:8081/auth/login', loginData);
+//       console.log(response.data);
+//       // Aquí podrías manejar la respuesta, por ejemplo, guardar el token en el estado global.
+//     } catch (error) {
+//       console.error('Error al iniciar sesión:', error.response.data.error);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <h2>Iniciar Sesión</h2>
+//       <form onSubmit={handleLogin}>
+//         <label>
+//           Username:
+//           <input type="text" name="username" value={loginData.username} onChange={handleChange} />
+//         </label>
+//         <br />
+//         <label>
+//           Password:
+//           <input type="password" name="password" value={loginData.password} onChange={handleChange} />
+//         </label>
+//         <br />
+//         <button type="submit">Iniciar Sesión</button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default Login;
